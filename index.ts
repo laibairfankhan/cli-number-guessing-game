6f3f1f9bbcccecc8a2c5bad44506188ec6c17f30
laibairfankhan <irfanlaiba978@gmail.com>
2024-03-23 08:00:52 -0700

    add in console

--- index.ts
+++ index.ts
@@ -2,18 +2,22 @@
 import inquirer from"inquirer";
 
 import chalk from "chalk";
-
+//Math.random generate a decimal value:
+//Math.floor use to convert  decimal value into integer:
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 
-console.log(chalk.bgCyanBright("wellcome to number guessing game."));
+console.log(chalk.bgBlueBright(`\n\n"wellcome to number guessing game."\n\n`));
 
 const anwers = await inquirer.prompt([
+    //Take a user number:
     {
         name:"userGuessedNumber",
         type: "number",
         message: "please guess a number between 1-10",
     }
 ]);
+
+// Test condition to check if the user guessed the correct number:
 if(anwers.userGuessedNumber === randomNumber){
     console.log(chalk.bgGreenBright("congratulation! you guessed right number."));
     
